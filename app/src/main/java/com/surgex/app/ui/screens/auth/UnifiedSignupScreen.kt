126081f1fde8e1604d26f340a2c1c4f71cddd9ef
tapietricks-blip <tapietricks@gmail.com>
2026-08-20 11:19:35 +0200

    Add missing tween import to UnifiedSignupScreen.kt

--- app/src/main/java/com/surgex/app/ui/screens/auth/UnifiedSignupScreen.kt
+++ app/src/main/java/com/surgex/app/ui/screens/auth/UnifiedSignupScreen.kt
@@ -1,6 +1,7 @@
 package com.surgex.app.ui.screens.auth
 
 import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
