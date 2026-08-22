493719f382f3d98e6c07468dce9319fa17296ba2
Tapie11 <72257327+Tapie11@users.noreply.github.com>
2026-08-22 03:12:57 +0200

    Fix: Add @OptIn(ExperimentalMaterial3Api::class) to DriverRideRequestScreen

--- app/src/main/java/com/surgex/app/ui/screens/driver/DriverRideRequestScreen.kt
+++ app/src/main/java/com/surgex/app/ui/screens/driver/DriverRideRequestScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.unit.sp
 /**
  * Driver ride request screen
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DriverRideRequestScreen(
     onAccept: () -> Unit,
